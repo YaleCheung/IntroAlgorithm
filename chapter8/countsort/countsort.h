77a1f4ed3c1b79b3ad4e6ee67149ff0b3a5a7f8c
zybjtu <zybjtu@gmail.com>
2014-10-27 20:52:58 +0800

    Complete the test for count sort

--- chapter8/countsort/countsort.h
+++ chapter8/countsort/countsort.h
@@ -19,27 +19,37 @@
 // count sort;
 // only can be used for int type
 
-void countsort(int data_array[], int output_array[], int ele_num) {
-    // assert data in range(-9999, 9999)
-    int max = -9999; 
+#include <assert.h>
+void countsort(int data_array[], int sorted_array[], int ele_num) {
+    // assert data in range(0, 9999) negative value could be used countsort after position move;
+    int max = 0; 
     int min = 9999;
+    int count_size;
+    int* count_array;
     for(auto i = 0; i < ele_num; i ++) {
         // in fact here is O(n);
+        assert(data_array[i] < 9999 && data_array[i] > -9999);
         if(data_array[i] > max)
             max = data_array[i];
 	if(data_array[i] < min)
             min = data_array[i];
 	// init the array;
 	count_size = max - min + 1;
-        int count_array[] = new[count_size];
+        count_array = new int[count_size];
     }
     // init count_array;
     for(auto i = 0; i < count_size; i ++)
         count_array[i] = 0;
     // count the data
     for(auto i = 0; i < ele_num; i ++)
-        count_array[data_array[i]] += 1;
+        count_array[data_array[i] - min] += 1;
     for(auto i = 1; i < count_size; i ++)
         count_array[i] = count_array[i] + count_array[i - 1];  // count_array[i] += count[i - 1];
-    
+    for(auto i = 0; i < ele_num; i ++) {
+        sorted_array[count_array[data_array[i] - min]] = data_array[i];
+        // for the same value;
+        count_array[data_array[i]] -= 1;
+    }
+    delete count_array;
+    return;
 }
--- chapter8/countsort/test.cc
+++ chapter8/countsort/test.cc
@@ -15,4 +15,21 @@
  *
  * =====================================================================================
  */
-
+#include "countsort.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(int argc, char* argv[]) {
+    int* input = new int[100];
+    int* output = new int[100];
+    srand(time(NULL));
+    for(auto i = 0; i < 100; i ++) {
+         int value = rand() % 50;
+         input[i] = value;
+    }
+    countsort(input, output, 100);
+    for(auto i = 0; i < 100; i ++)
+        std::cout << input[i] << " " << output[i] << std::endl;
+    return 0;
+}
