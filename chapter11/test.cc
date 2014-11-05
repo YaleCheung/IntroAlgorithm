62e0bfb2ba741cc4805ea0c8d7bea6e0a29d74ae
zybjtu <zybtju@gmail.com>
2014-11-05 13:40:29 +0800

    add test file

---
+++ chapter11/test.cc
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/05/14 13:38:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "direct_address_array.h"
+#include <stdio.h>
+int main(int argc, cha* argv[]) {
+    Array();
+    return 0;
+}
+
