67d32d4d8dc201c2bae05137dab6c100e32ede0d
zybjtu <zybjtu@gmail.com>
2014-11-06 13:52:38 +0800

    modify the hash_link

--- chapter11/linked_hash.h
+++ chapter11/linked_hash.h
@@ -28,9 +28,51 @@ public:
     LinkHash();
     Hash& Insert(const T&);
     Hash& Delete(const T&);
+    virtual int Hash_Func(const T&) const;
     bool Find() const;
     
 private:
-    Link* array_; //  an array to store the head of link;
+    void Init_();
+    Link<T>* array_; //  an array to store the head of link;
+};
+
+template<typename T>
+LinkHash<T>::LinkHash(const int& size = 100) :
+    size_(size), array_(NULL) {
+    Init_();
 }
+
+template<typename T>
+LinkHash<T>::Init_() {
+    array_ = new Link<T>[size_];
+}
+
+template<typename T>
+LinkHash<T>& Insert(const T& val) {
+    int pos = Hash_Func(val);
+    assert(pos < size_);
+    array_[pos].Insert(val, 1);
+}
+
+template<typename T>
+LinkHash<T>& Delete(const T& val) {
+    int pos = Hash_Func(val);
+    assert(pos < size_);
+    int size = array_[pos].Size();
+    if(size > 0) {
+        int val_pos = array_[pos].Search(val);
+        assert(val_pos > 0);
+        array_[pos].Delete(val_pos);
+    }
+    return *this;
+}
+
+template<typename T>
+bool Find(const T& val) {
+    int pos = Hash_Func(val);
+    assert(pos < size_);
+    return array[pos_].Find(val);
+}
+
+
 #endif // linked hash
