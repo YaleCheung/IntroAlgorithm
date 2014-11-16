de228290714f00ebce13106bab7b2523cf118114
zybjtu <zybjtu@google.com>
2014-11-16 22:58:39 +0800

    add git tag

--- chapter12/binarysearchtree.h
+++ chapter12/binarysearchtree.h
@@ -16,24 +16,68 @@
  * =====================================================================================
  */
 
+// something about search tree
 #ifndef BINARY_SEARCH_TREE_H
 #define BINARY_SEARCH_TREE_H
 
 #include "../lib/nocopyable.h"
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+template<typename T>
+class BinarySearchTree;
+
+template<typename T>
+class Node {
+public:
+    friend BinarySearchTree<T>; 
+    Node();
+    Node(const T& val, Node* left, Node* right);
+    ~Node();
+private:
+    T data_;
+    Node* left_;
+    Node* right_;
+};
+
+template<typename T>
+Node<T>::Node() :
+    left_(NULL), right_(NULL) { 
+}
+
+template<typename T>
+Node<T>::Node(const T& val, Node* left, Node* right) :
+    data_(val), left_(left), right_(right) {}
+
 template<typename T>
 class BinarySearchTree : NoCopyable<T> {
 public:
+    typedef void (*Func)(Node<T>*);
     BinarySearchTree();
+    BinarySearchTree(const T val, const BinarySearchTree& l, const BinarySearchTree& r);
     BinarySearchTree& Insert(const T& val);
     BinarySearchTree& Delete(const T& val);
+
     bool Find(const T& val);
+    void InOrderR(Func func);
+    void LastOrderR(Func func);
+    void FirstOrderR(Func func);
+    void InOrderI(Func func);
+    void LastOrderI(Func func);
+    void FirstOrderI(Func func);
 private:
     void Init_();
-    
-    typedef struct Node {
-        T data;
-        Node* p_left;
-        Node* p_right;
-    } Node, *pNode;
-    pNode p_root_;
+    Node<T>* root_;
 };
+
+template<typename T>
+BinarySearchTree<T>::BinarySearchTree() :
+    root_(NULL) {
+   Init_();
+};
+
+template<typename T>
+void BinarySearchTree<T>::
+#endif //BINARY_SEARCH_TREE_H
