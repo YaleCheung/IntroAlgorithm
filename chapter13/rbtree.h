22e90a794837b58cb0a2703453fc22603038ded1
zybjtu <zybjtu@gmail.com>
2014-12-01 23:15:22 +0800

    the basic frame of rbtree

--- chapter13/rbtree.h
+++ chapter13/rbtree.h
@@ -16,4 +16,47 @@
  * =====================================================================================
  */
 
+#ifndef RBTree_H 
+#define RBTree_H 
 
+#include <iostream>
+#include "../lib/nocopyable.h"
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+template<typename T>
+class RBTree;
+
+template<typename T>
+class Node {
+public:
+    friend RBTree<T>; 
+    Node();
+    Node(const T& val, Node* left, Node* right, Node* parent);
+    ~Node();
+private:
+    T data_;
+    Node* left_;
+    Node* right_;
+    Node* parent_;
+};
+
+template<typename T>
+Node<T>::Node() :
+    left_(NULL), right_(NULL), parent_ { 
+}
+
+template<typename T>
+Node<T>::Node(const T& val, Node* left, Node* right) :
+    data_(val), left_(left), right_(right) {}
+template<typename T>
+Node<T>::~Node(){}
+
+
+template<typename T>
+class RBTree {
+    
+};
+#endif
